24415c0e0be5953fcc52a5c9a2b7f6881acb47f8
SIVASANKARYA <sivasankarmca39@gmail.com>
2024-03-11 16:42:59 +0530

    Your commit message here

--- siva.tf
+++ siva.tf
@@ -2,17 +2,16 @@ provider "aws" {
   region = "us-east-1"  # Set your desired AWS region
 }
 
-resource "aws_db_instance" "example_rds" {
-  identifier            = "example-rds-instance"
+resource "aws_db_instance" "sivarajakumar" {
+  identifier            = "sivarajakumar"
   allocated_storage     = 20
   storage_type          = "gp2"
   engine                = "mysql"
-  engine_version        = "5.7"
-  instance_class        = "db.t2.micro"
-  name                  = "sivasankar"
-  username              = "admin"
+  engine_version        = "8.0.33"
+  instance_class        = "db.t3.micro"
+   username              = "administrator"
   password              = "Passworesdfff4"  # Please set a strong password
-  parameter_group_name  = "default.mysql5.7"
+  parameter_group_name  = "default.mysql8.0"
   
   # You can customize the configuration according to your needs
 }
